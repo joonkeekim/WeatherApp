6bb26c8b8cb957774d8de2ef44a898d7953e907c
joonkeekim <joonkee0701@gmail.com>
2020-03-23 22:14:50 +0900

    2

--- Loading.js
+++ Loading.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Loading(){
     return( 
     <View style={styles.container}>
-        <Text>getting the weather now</Text>
+        <Text style={styles.text}>getting the weather now!</Text>
     </View>
     );
 }
@@ -13,6 +13,12 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent: "flex-end",
-        padding: 40 
+        paddingHorizontal: 40,
+        paddingVertical : 100,
+        backgroundColor: "#FDF6AA" 
+    },
+    text:{
+        color: "#2c2c2c",
+        fontSize : 30
     }
 });
